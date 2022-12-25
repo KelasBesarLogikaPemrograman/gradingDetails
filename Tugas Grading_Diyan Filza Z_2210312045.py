cfce70cd03e14d6d1cda50e54aae7cb6be5c69d7
DiyanFilzaZ <diyanfilza@gmail.com>
2022-12-25 13:08:57 +0700

    Tugas gradingDetails-Diyan Filza Z(2210312045)

---
+++ Tugas Grading_Diyan Filza Z_2210312045.py
@@ -0,0 +1,58 @@
+username_1 = "filza"
+password_1 = "satuduatiga123"
+
+print ("please insert your username and password to login:")
+user_name = input("Username :")
+pass_word = input("Password :")
+print ("\n")
+
+if user_name == username_1 and pass_word == password_1 :
+    print ("Status Login = Active")
+    print ("Hallo, " + user_name)
+    print ("\n")
+    nama_mahasiswa = input("Masukkan Nama Mahasiswa yang akan di grading : ")
+    nama_matkul = input("Nama Mata Kuliah : ")
+    Nilai_Presensi = input("Silahkan Masukkan nilai Presensi : ")
+    Nilai_Tugas = input("Silahkan Masukkan nilai Tugas : ")
+    Nilai_UTS = input("Silahkan Masukkan nilai UTS : ")
+    Nilai_UAS = input("Silahkan Masukkan nilai UAS : ")
+    def rata_rata_nilai(Nilai_Presensi, Nilai_Tugas, Nilai_UTS, Nilai_UAS):
+        total_nilai = 0.05 * float(Nilai_Presensi) + 0.1 * float(Nilai_Tugas) + 0.30 * float(Nilai_Tugas) + 0.25 * float(Nilai_UTS) + 0.30 * float(Nilai_UAS)
+        if 100 >= total_nilai >= 85 :
+            return "A"
+        if total_nilai >= 70 :
+            return "B"
+        if total_nilai >= 50 :
+            return "C"
+        if total_nilai >= 30 :
+            return "D"
+        else :
+            return "E"
+    X = nama_mahasiswa
+    Y = (rata_rata_nilai(Nilai_Presensi, Nilai_Tugas, Nilai_UTS, Nilai_UAS))
+    Z = nama_matkul
+    total_nilai = (0.05 * float(Nilai_Presensi) + 0.1 * float(Nilai_Tugas) + 0.30 * float(Nilai_Tugas) + 0.25 * float(Nilai_UTS) + 0.30 * float(Nilai_UAS))
+    print ("\n")
+    print ("Mahasiswa", X, "mendapatkan nilai", Y, "untuk Mata Kuliah", Z, "dengan skor total", total_nilai)
+else :
+    print ("your username or password is wrong, please try again")
+
+#OUTPUT
+#please insert your username and password to login:
+#Username :filza
+#Password :satuduatiga123
+
+
+#Status Login = Active
+#Hallo, filza
+
+
+#Masukkan Nama Mahasiswa yang akan di grading : Diyan Filza Zahira
+#Nama Mata Kuliah : Logika Pemograman  
+#Silahkan Masukkan nilai Presensi : 100
+#Silahkan Masukkan nilai Tugas : 100
+#Silahkan Masukkan nilai UTS : 100
+#Silahkan Masukkan nilai UAS : 100
+
+
+#Mahasiswa Diyan Filza Zahira mendapatkan nilai A untuk Mata Kuliah Logika Pemograman  dengan skor total 100.0
